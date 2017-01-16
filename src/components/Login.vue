837755bbea32fa357d09733b1473cc0df96d108b
wqc <15000750457@163.com>
2017-01-16 16:28:17 +0800

    登录

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,58 @@
 <template>
   <div>
-    <input/>
-    <input/>
+    <input v-model="name"/>
+    <input type="password" v-model="password"/>
+    <input type="button" value="登录" @click="login" />
+    <input type="button" value="注册并登录" @click="registerAndLogin" />
   </div>
 </template>
 <script>
 export default{
+  data(){
+    return {
+      name : null,
+      password : null
+    }
+  },
+  methods : {
+    login(){
+      var name = this.name;
+      var password = this.password;
+      wilddog.auth().signInWithEmailAndPassword(name,password).then(function(user){
+        // 获取用户
+        console.log(user);
+        //配置用户管理信息
+        this.setUserInfo(user.uid);
+      }).catch(function (error) {
+        // 错误处理
+        alert(error);
+      });
+    },
+    registerAndLogin(){
+      var name = this.name;
+      var password = this.password;
+      wilddog.auth().createUserWithEmailAndPassword(name,password).then(function(user){
+        // 获取用户
+        console.log(user);
+        //配置用户管理信息
+        this.setUserInfo(user.uid);
+      }).catch(function (error) {
+        // 错误处理
+        alert(error);
+      });
+    }
+  },
+  setUserInfo(uid){
+    var ref = wilddog.sync().ref().child(uid);
+  },
   beforeCreate(){
-    console.log('========================================');
+    console.log('=================初始化wilddog连接=======================');
+    var config = {
+      authDomain: "draw-and-guess.wilddog.com",
+      syncURL: "https://draw-and-guess.wilddogio.com"
+    }
+    wilddog.initializeApp(config)
+    console.log('=================初始化wilddog连接完毕=======================');
   }
 }
 </script>
