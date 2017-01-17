58b082873f02878f8eb066cab97cbf1685817983
wqc <15000750457@163.com>
2017-01-17 16:14:22 +0800

    添加Element-Ui框架，修改登录模块样式，新增大厅组件

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>
-    <input v-model="name"/>
-    <input type="password" v-model="password"/>
-    <input type="button" value="登录" @click="login" />
-    <input type="button" value="注册并登录" @click="registerAndLogin" />
+  <div class="loginBox">
+    <el-row>
+      <el-input type="text" placeholder="请输入内容" v-model="name">
+        <template slot="prepend">邮箱</template>
+      </el-input>
+    </el-row>
+    <el-row>
+      <el-input type="password" placeholder="请输入内容" v-model="password">
+        <template slot="prepend">密码</template>
+      </el-input>
+    </el-row>
+    <el-row>
+      <el-col :span="3" :push="6">
+        <el-button type="primary"  @click="login" >登录</el-button>
+      </el-col>
+      <el-col :span="3" :push="8">
+        <el-button type="primary"  @click="registerAndLogin" >注册并登录</el-button>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
@@ -16,26 +30,36 @@ export default{
   },
   methods : {
     login(){
-      var name = this.name;
-      var password = this.password;
+      var _this = this;
+      var name = _this.name;
+      var password = _this.password;
       wilddog.auth().signInWithEmailAndPassword(name,password).then(function(user){
-        // 获取用户
-        console.log(user);
+        _this.redirect();
       }).catch(function (error) {
         // 错误处理
         alert(error);
       });
     },
     registerAndLogin(){
-      var name = this.name;
-      var password = this.password;
+      var _this = this;
+      var name = _this.name;
+      var password = _this.password;
       wilddog.auth().createUserWithEmailAndPassword(name,password).then(function(user){
-        // 获取用户
-        console.log(user);
+        _this.initUserInfo(user.uid)
+        _this.redirect()
       }).catch(function (error) {
         // 错误处理
         alert(error);
       });
+    },
+    redirect(){
+      router.push('./hall');
+    },
+    initUserInfo(uid){
+      var ref = wilddog.sync().ref("/").child(uid);
+      ref.child('win').set(0);
+      ref.child('fail').set(0);
+      ref.child('escape').set(0);
     }
   },
   beforeCreate(){
@@ -49,3 +73,7 @@ export default{
   }
 }
 </script>
+<style>
+  .loginBox{position: absolute;width:400px;margin-left:-200px;top:100px;left:50%;border:1px solid #ccc;border-radius:20px;padding:40px;}
+  .el-row:not(:last-child){margin-bottom:20px;}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
-import WildDog from 'wilddog'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
 import Login from './components/Login.vue'
+import Hall from './components/Hall.vue'
 import './css/style.css'
 
 // 调用模块
 Vue.use(VueResource)
 Vue.use(VueRouter)
+Vue.use(ElementUI)
 
 require('wilddog')
 
@@ -22,14 +25,14 @@ const routes = [
 const router = new VueRouter({
   routes: [
     {path : '/login',component: Login},
+    {path : '/hall',component: Hall},
     {path: '/', redirect: '/login'}
   ]
 })
+window.router = router
 
 // 创建和挂载根实例。
 new Vue({
   el : '#app',
   router
 })
-
-router.push('/login');
