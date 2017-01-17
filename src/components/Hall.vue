0e4d986edcd40384a6c5666c83e29a1c13de5e2b
wqc <15000750457@163.com>
2017-01-17 18:05:01 +0800

    新增大厅组件

---
+++ src/components/Hall.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <!--<el-row>-->
+      <!--<el-col :span="3" :push="6">-->
+        <!--<el-button type="primary"  @click="login" >Create Room</el-button>-->
+      <!--</el-col>-->
+    <!--</el-row>-->
+    <div class="hall">
+      <!--<div v-for="room in rooms">-->
+      <!--{{ item.text }}-->
+      <!--</div>-->
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return {
+        demo : {
+          index : 1,
+          created : new Date(),
+          name : '死亡笔记'
+        },
+        rooms : []
+      }
+    },
+    methods : {
+    },
+    created(){
+      console.log('=================初始化大厅=======================');
+      this.rooms = [];
+      var ref = wilddog.sync().ref("/").child('roomList');
+      ref.remove();
+      for(var i=1;i<=1000;i++){
+        ref.push({
+          index : i,
+          created : new Date().getTime(),
+          name : '死亡笔记'+i
+        });
+      }
+//      wilddog.sync().ref("/").child('roomList').on('child_added',(snapshot,error) => {
+//          var room = snapshot.val()
+//          console.log(room);
+////        this.rooms.push({
+////        })
+//      })
+      console.log('=================初始化大厅完毕=======================');
+    }
+  }
+//  var room = {
+//    name : 'room_name',
+//    max : 'max_player_in',
+//    users : ['uid1','uid2','uid3']
+//  };
+
+
+</script>
